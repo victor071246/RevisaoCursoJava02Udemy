d19208f9a82f33549427db06e0b624d88772ef0a
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-11-12 23:52:39 -0300

    Add files via upload

---
+++ fundamentos/Comentario.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class Comentario {
+    /*
+    * Classe responsável por mostrar o uso dos 3 tipos de comentário que o java suporta  Esse método é a porta de entrada de um programa Java.
+    *
+    */
+    public static void main(String[] args) {
+        /*
+        * Imprime no console a palavra 'Primeiro' e em seguida cria uma nova linha
+        */
+        System.out.println("Primeiro");
+
+        // Cuidado com os comentários óbvios
+        System.out.println("Programa!");
+    }
+}
